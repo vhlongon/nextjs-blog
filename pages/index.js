c23cd357febd691a2dd47074d4dfbd79f3aa8484
victor longon <vhlongon@gmail.com>
2020-11-22 19:44:13 +0100

    list posts in index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout, { siteTitle } from "../components/Layout";
 import * as utilStyles from "../styles/utils";
 import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/Date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -30,11 +32,13 @@ const Home = ({ allPostsData }) => {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small css={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
